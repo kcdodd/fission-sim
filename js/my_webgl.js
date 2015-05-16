e75ef2ca54a374dd3ab08d55b71e37d4f50bcb0a
kcdodd <carter.dodd@gmail.com>
2015-05-15 21:40:35 -0500

    webgl

--- js/my_webgl.js
+++ js/my_webgl.js
@@ -13,20 +13,35 @@ var WebGL = {
 
         var out = {};
 
-        out.canvas = document.getElementById(canvas_id);
+        var canvas = document.getElementById(canvas_id);
 
-        if (!out.canvas) {
-            throw "canvas context undefined.";
+        if (!canvas) {
+            throw new Error("canvas context undefined.");
         }
 
+        Object.defineProperty(out, 'canvas', {
+            configurable: true,
+            get: function() {
+                return canvas;
+            }
+        });
 
-        out.gl = out.canvas.getContext("webgl");
 
-        if (!out.gl) {
-            throw "webgl context undefined.";
+        var gl = out.canvas.getContext("webgl");
+
+        if (!gl) {
+            throw new Error("webgl context undefined.");
         }
 
-        out.shaders = [];
+        Object.defineProperty(out, 'gl', {
+            configurable: true,
+            get: function() {
+                return gl;
+            }
+        });
+
+
+        var shaders = [];
 
         out.addShader = function(type, source) {
             // compile shader sources
@@ -35,63 +50,160 @@ var WebGL = {
 
             if (type === "vertex" || type === "x-shader/x-vertex") {
 
-                shader = out.gl.createShader(out.gl.VERTEX_SHADER);
+                shader = gl.createShader(gl.VERTEX_SHADER);
 
             }else if (type === "fragment" || type === "x-shader/x-fragment") {
 
-                shader = out.gl.createShader(out.gl.FRAGMENT_SHADER);
+                shader = gl.createShader(gl.FRAGMENT_SHADER);
 
             }else{
-                throw "Shader type " + type + " undefined.";
+                throw new Error("Shader type " + type + " undefined.");
             }
 
-            out.gl.shaderSource(shader, source);
+            gl.shaderSource(shader, source);
 
-            out.gl.compileShader(shader);
+            gl.compileShader(shader);
 
-            var compiled = out.gl.getShaderParameter(shader, out.gl.COMPILE_STATUS);
+            var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
 
             if (!compiled) {
 
-              var error = out.gl.getShaderInfoLog(shader);
-              out.gl.deleteShader(shader);
+              var error = gl.getShaderInfoLog(shader);
+              gl.deleteShader(shader);
 
-              throw "Shader compilation failed: " + error;
+              throw new Error("Shader compilation failed: " + error);
             }
 
-            out.shaders.push(shader);
+            shaders.push(shader);
         };
 
-        out.program = null;
+        var program = null;
+
+        Object.defineProperty(out, 'program', {
+            configurable: true,
+            get: function() {
+                return program;
+            }
+        });
 
         out.linkProgram = function() {
 
-            if (out.program) {
-                out.gl.deleteProgram(out.program);
+            if (program) {
+                gl.deleteProgram(program);
             }
 
-            out.program = out.gl.createProgram();
+            program = gl.createProgram();
 
-            out.shaders.forEach(function(shader) {
-                out.gl.attachShader(out.program, shader);
+            shaders.forEach(function(shader) {
+                gl.attachShader(program, shader);
             });
 
-            out.gl.linkProgram(out.program);
+            gl.linkProgram(program);
 
 
-            var linked = out.gl.getProgramParameter(out.program, out.gl.LINK_STATUS);
+            var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
 
             if (!linked) {
-                var error = out.gl.getProgramInfoLog(out.program);
+                var error = gl.getProgramInfoLog(program);
 
-                out.gl.deleteProgram(out.program);
+                gl.deleteProgram(program);
 
-                out.program = null;
+                program = null;
 
-                throw "Program linking failed: " + error;
+                throw new Error("Program linking failed: " + error);
             }
 
-            out.gl.useProgram(out.program);
+            gl.useProgram(program);
+        };
+
+        var textures = [];
+
+        out.enableFloatTexture = function(){
+            var OES_texture_float = gl.getExtension('OES_texture_float');
+
+            if (!OES_texture_float) {
+                throw new Error("No support for OES_texture_float");
+            }
+        };
+
+        out.addTexture = function(width, height, image, useFloat) {
+
+            var texture = gl.createTexture();
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+            if (useFloat) {
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, image);
+            }else{
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            }
+
+            textures.push(texture);
+
+            gl.bindTexture(gl.TEXTURE_2D, null);
+
+            return texture;
+
+        };
+
+        var framebuffers = [];
+
+        out.addFrameBuffer = function(width, height, useFloat) {
+
+            var texture = out.addTexture(width, height, null, useFloat);
+
+            //out.gl.bindTexture(gl.TEXTURE_2D, texture);
+
+            var fbo = gl.createFramebuffer();
+            var fb = {
+                index : framebuffers.size(),
+                frameBuffer: fbo,
+                texture : texture,
+                width : width,
+                height : height};
+
+            framebuffers.push(fb);
+
+            gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+            // Attach a texture to it.
+            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+
+            if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE) {
+                    throw new Error("Frame buffer creation failed");
+            }
+
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+            return fb;
+        };
+
+        out.bindFrameBuffer = function(i) {
+            var fb = framebuffers[i];
+
+            // make this the framebuffer we are rendering to.
+            gl.bindFramebuffer(gl.FRAMEBUFFER, fb.frameBuffer);
+
+            // Tell the shader the resolution of the framebuffer.
+            var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+            gl.uniform2f(resolutionLocation, fb.width, fb.height);
+
+            // Tell webgl the viewport setting needed for framebuffer.
+            gl.viewport(0, 0, fb.width, fb.height);
+        };
+
+        out.bindCanvas = function () {
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+            // Tell the shader the resolution of the framebuffer.
+            var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+            gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+            // Tell webgl the viewport setting needed for framebuffer.
+            gl.viewport(0, 0, canvas.width, canvas.height);
         };
 
         return out;
--- js/rgba_math.js
+++ js/rgba_math.js
@@ -1,36 +1,92 @@
 var RGBA_MATH = {
     rgba_to_float : function(rgba) {
-        return rgba[0]/255.0 + rgba[1]/65280.0 + rgba[2]/16711680.0 + rgba[2]/4278190080.0;
+        return rgba[0]/255.0 + rgba[1]/65280.0 + rgba[2]/16711680.0 + rgba[3]/4278190080.0;
     },
 
     // x \in [0.0, 1.0]
     float_to_rgba : function (x) {
         var rgba = new Uint8ClampedArray(4);
 
-        x *= 255;
+        x *= 255.0;
 
         for(var i = 0; i < 4; i++) {
             //console.log(x);
             rgba[i] = Math.floor(x);
-            x = x - rgba[i];
-            x *= 256;
+            x = (x - rgba[i])*256.0;
         }
 
         return rgba;
 
+    },
+
+    add : function (a, b) {
+        var result = new Uint8Array(4);
+
+        var tmp = 0;
+
+        tmp = a[3] + b[3];
+        result[3] = tmp;
+
+        tmp = a[2] + b[2] + (tmp >>> 8);
+        result[2] = tmp;
+
+        tmp = a[1] + b[1] + (tmp >>> 8);
+        result[1] = tmp;
+
+        tmp = a[0] + b[0] + (tmp >>> 8);
+        result[0] = tmp;
+
+        return result;
+    },
+
+    subtract : function(a,b) {
+
+    },
+
+    multiply : function(a, b) {
+        var mask = 0x000000FF;
+
+        var result = new Uint8Array(4);
+
+        // 16 cross terms in multiplying two rgba numbers, but drop lowest 2 orders
+        var term0 = a[0]*b[0];
+        var term1 = a[0]*b[1] + a[1]*b[0];
+        var term2 = a[1]*b[1] + a[0]*b[2] + a[2]*b[0];
+        var term3 = a[0]*b[3] + a[3]*b[0] + a[1]*b[2] + a[2]*b[1];
+        var term4 = a[1]*b[3] + a[3]*b[1] + a[2]*b[2];
+
+        var red = term0 + (term1 >>> 8);
+        result[0] = red/255;
+        red = red - 255*result[0];
+
+        var green = (red << 8) + (mask & term1) + (term2 >>> 8);
+        result[1] = green/255;
+        green = green - 255*result[1];
+
+        var blue = (green << 8) + (mask & term2) + (term3 >>> 8);
+        result[2] = blue/255;
+        blue = blue - 255*result[2];
+
+        var alpha = (blue << 8) + (mask & term3) + (term4 >>> 8);
+
+        result[3] = alpha/255;
+
+        return result;
     }
 };
 
-var x = Math.PI/4.0;
+var x = RGBA_MATH.rgba_to_float(RGBA_MATH.float_to_rgba(0.12111238885));
 
-console.log(x);
+var a = RGBA_MATH.float_to_rgba(x);
 
-var rgba = RGBA_MATH.float_to_rgba(x);
 
-console.log(rgba);
+var c =  RGBA_MATH.add(a,a);
 
-var y = RGBA_MATH.rgba_to_float(rgba);
 
-console.log(y);
+var z = RGBA_MATH.rgba_to_float(c);
 
-console.log(Math.abs(x -y));
+console.log(a);
+console.log(c);
+
+console.log("float " + 2*x);
+console.log("rgba  " + z);
